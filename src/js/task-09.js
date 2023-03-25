51dac26a78319b12f817fa7836c86318e6563681
Wojciech Dutkiewicz <dutwoj95@gmail.com>
2023-03-25 13:30:00 +0100

    ok

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,12 @@
+const bodyBg = document.body;
+const colorEl = document.querySelector('.color');
+const colorChangeEl = document.querySelector('.change-color');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+colorChangeEl.addEventListener('click', () => {
+  bodyBg.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
